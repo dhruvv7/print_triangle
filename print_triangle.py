7998173f5f5cabc5d265ba8508e44246bcf77690
Cloud User <ec2-user@ip-172-31-23-54.us-east-2.compute.internal>
2018-07-03 14:21:07 +0000

    WIP: Started with input parameters

--- print_triangle.py
+++ print_triangle.py
@@ -10,3 +10,30 @@
 #in branch
 
 #edited the file from github UI, essentially a location other than my terminal
+
+#Now the programming starts. Will keep above comments for historic reasons
+
+import sys
+
+triangle = 0 # variable to hold choice of right or equilateral 
+rows = 3 # variable to hold number of rows
+
+#if user has given command line parameters, do sanity check and assign it to correspoinding varibles
+#or stick to default values
+if (sys.len > 0):
+	temp = int(sys.argv[0]) #convert to int
+
+	if (isinstance(temp,int) == True) #check if temp is indeed int
+		triangle = temp
+
+	if ( triangle != 0 || triangle != 1)
+		triangle = 0
+
+
+print (sys.argv)
+
+#call function to print triangle
+
+#end
+
+#function to print triangle. Parameters are type and number of rows
