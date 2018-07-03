6185d43d53aa58ffff1ad86617f11ccc4f3f9ea0
Cloud User <ec2-user@ip-172-31-23-54.us-east-2.compute.internal>
2018-07-03 11:30:26 +0000

    First commit

---
+++ print_triangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+#Author: Dhruv Desai
+
+#This is the code for printing traingle on screen
+#The size and type is dependent on command line arguments
+#
+#Usage: python3 print_triangle <size_in_integer> <type: 0 for right, 1 for equilateral>
+#       Both the arguments are optional. 
+
+
